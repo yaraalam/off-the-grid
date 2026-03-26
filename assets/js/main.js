0e26fe3bfc3a52f9b149b9a570b9f173859c9d5a
yara <52312801+yaraalam@users.noreply.github.com>
2026-03-26 20:40:13 +0000

    added flame background and updated js code for rotation animations on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 // main.js
 
-console.log("Hello, World!");
+let rotation = 0;
+document.addEventListener("click", function() {
+    let min = 10;
+    let max = 40;
+    rotation += Math.floor(Math.random() * (max - min + 1)) + min;
+    document.body.style.transform = `rotate(${rotation}deg)`;
+});
