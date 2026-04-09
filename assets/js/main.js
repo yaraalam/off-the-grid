e2014d23a5bd24a4350da3af40930e93b484c49b
yara <52312801+yaraalam@users.noreply.github.com>
2026-04-09 18:06:09 +0000

    fixed main.js types and syntax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,39 @@
 // main.js
+let divs = document.querySelectorAll(".grid-container div");
 
-let rotation = 0;
 document.addEventListener("click", function() {
-    let min = 10;
-    let max = 40;
+    let rotation = randomNumber(10, 40);
     document.body.style.transform = `rotate(${rotation}deg)`;
+    randomize();
 });
 
-function randomNumber (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) * min;
-}
-let divs = document.querySelectorAll (".grid-container div");
-function randomize(){
-    console.log(divs.length, "randomize!'")
-}
-function randomize (){
-    divs.forEach(function(div){
-        let scale = randomNumber (.5, 1.2);
-        let translateX = randomNumber (0, 50);
-        let translateY = randomNumber (0, 20);
-        let rotate = randomNumber (0, 360);
-        div.style.transform =
-            `scale(${scale})
-            translate(${translateX}%, &{translateY}%)
-            rotate(${rotate}deg)`;
-    })
-    console.log(divs.length, "randomize! ");
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(square(2)); 
+function randomFloat(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function randomize() {
+    divs.forEach(function(div) {
+        let scale = randomFloat(0.5, 1.2);
+        let translateX = randomNumber(0, 50);
+        let translateY = randomNumber(0, 20);
+        let rotate = randomNumber(0, 360);
+
+        div.style.transform = 
+            `scale(${scale}) 
+             translate(${translateX}%, ${translateY}%) 
+             rotate(${rotate}deg)`;
+    });
+
+    console.log(divs.length, "randomize!");
+}
+
+function square(num) {
+    return num * num;
+}
+
+console.log(square(2));
 console.log(square(10));
