9ba0b56f84c0387bddf22f2300647177534ae5b0
yara <52312801+yaraalam@users.noreply.github.com>
2026-04-09 17:59:31 +0000

    updated with own haiku and gif but working on js main

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,29 @@ let rotation = 0;
 document.addEventListener("click", function() {
     let min = 10;
     let max = 40;
-    rotation += Math.floor(Math.random() * (max - min + 1)) + min;
     document.body.style.transform = `rotate(${rotation}deg)`;
 });
+
+function randomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) * min;
+}
+let divs = document.querySelectorAll (".grid-container div");
+function randomize(){
+    console.log(divs.length, "randomize!'")
+}
+function randomize (){
+    divs.forEach(function(div){
+        let scale = randomNumber (.5, 1.2);
+        let translateX = randomNumber (0, 50);
+        let translateY = randomNumber (0, 20);
+        let rotate = randomNumber (0, 360);
+        div.style.transform =
+            `scale(${scale})
+            translate(${translateX}%, &{translateY}%)
+            rotate(${rotate}deg)`;
+    })
+    console.log(divs.length, "randomize! ");
+}
+
+console.log(square(2)); 
+console.log(square(10));
